8398b9a96572b981e8465c010d04df4af0fb702c
natasilva <nata.software@hotmail.com>
2024-09-21 15:12:01 -0300

    feat: Add react-native-elements

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet, Card, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import { Card } from 'react-native-elements';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Card>
-      <Card.Title>LOGIN</Card.Title>
+        <Card.Title>LOGIN</Card.Title>
         <Text style={styles.text}>LOGIN</Text>   
         <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />   
+          style={styles.input}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />   
         <Button
-        title="ENTRAR"
-        onPress={() => navigation.navigate('Results')}
+          title="ENTRAR"
+          onPress={() => navigation.navigate('Results')}
         />
       </Card>
     </View>
