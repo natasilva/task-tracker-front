d9e144a7c6d1705965433de80284e63229c53afe
renanmartins96 <renan.martins764@hotmail.com>
2024-10-30 19:56:42 -0300

    feat: Add tela de login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-import { Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Card } from 'react-native-elements';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
+  const [CAIXA, setCaixa] = useState('');
+  const [senha, setSenha] = useState('');
+
   return (
     <View style={styles.container}>
-      <Card>
+      <Card containerStyle={styles.card}>
+        <Image
+          source={require('../../assets/pac.png')}
+          style={styles.image}
+        />
+
         <Card.Title>LOGIN</Card.Title>
-        <Text style={styles.text}>LOGIN</Text>   
+        
         <TextInput
           style={styles.input}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="useless placeholder"
+          placeholder="CAIXA"
+          value={CAIXA}
+          onChangeText={(text) => setCaixa(text)}
           keyboardType="numeric"
-        />   
-        <Button
-          title="ENTRAR"
-          onPress={() => navigation.navigate('Results')}
         />
+
+        <TextInput
+          style={styles.input}
+          placeholder="SENHA"
+          value={senha}
+          onChangeText={(text) => setSenha(text)}
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity
+          style={styles.rectangularButton}
+          onPress={() => navigation.navigate('Results')}
+        >
+          <Text style={styles.buttonText}>ENTRAR</Text>
+        </TouchableOpacity>
       </Card>
     </View>
   );
@@ -34,8 +51,43 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 20,
+  image: {
+    marginBottom: 50,
+    marginEnd: "auto",
+    marginStart: "auto",    
+    width: 150,
+    height: 60,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: 170,
+    alignSelf: 'center',
+    textAlign: 'center',
+    borderRadius: 5,
+  },
+  card: {
+    width: 350,
+    height: 400,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rectangularButton: {
+    width: 100,
+    height: 50,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4CAF50',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
