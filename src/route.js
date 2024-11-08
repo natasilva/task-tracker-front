00b2fda0d870469c70de36eef58bf7efc6ddd391
renanmartins96 <renan.martins764@hotmail.com>
2024-11-07 21:13:27 -0300

    feat: Add botao de logout

--- src/route.js
+++ src/route.js
@@ -6,40 +6,53 @@ import LoginScreen from './screens/LoginScreen';
 import ResultsScreen from './screens/ResultsScreen';
 import TargetReportScreen from './screens/TargetReportScreen';
 import TotalizersScreen from './screens/TotalizersScreen';
+import { TouchableOpacity, Text } from "react-native";
 
 const Stack = createStackNavigator();
 
 const Routes = () => {
   return (
-      <Stack.Navigator initialRouteName="Login">
-        {/* Rota para a tela de login */}
-        <Stack.Screen 
-          name="Login" 
-          component={LoginScreen} 
-          options={{ title: 'Login' }} 
-        />
+    <Stack.Navigator initialRouteName="Login">
+      {/* Rota para a tela de login */}
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ title: 'Login' }}
+      />
 
-        {/* Rota para a listagem de resultados diários */}
-        <Stack.Screen 
-          name="Results" 
-          component={ResultsScreen} 
-          options={{ title: 'Listagem de Resultados Diários' }} 
-        />
+      {/* Rota para a listagem de resultados diários */}
+      <Stack.Screen
+        name="Results"
+        component={ResultsScreen}
+        options={{ title: 'Listagem de Resultados Diários' }}
+      />
 
-        {/* Rota para o relatório de metas */}
-        <Stack.Screen 
-          name="TargetReport" 
-          component={TargetReportScreen} 
-          options={{ title: 'Relatório de Metas' }} 
-        />
+      {/* Rota para o relatório de metas */}
+      <Stack.Screen
+        name="TargetReport"
+        component={TargetReportScreen}
+        options={({ navigation }) => ({
+          title: 'Relatório de Metas',
+          headerRight: () => (
+            <TouchableOpacity
+              style={{ marginRight: 15 }}
+              onPress={() => {
+                navigation.replace('Login');
+              }}
+            >
+              <Text style={{ color: '#007bff', fontSize: 16 }}>Sair</Text>
+            </TouchableOpacity>
+          )
+        })}
+      />
 
-        {/* Rota para o cadastro de resultados */}
-        <Stack.Screen 
-          name="Register" 
-          component={TotalizersScreen}
-          options={{ title: 'Cadastro de Resultado', presentation: 'modal' }} 
-        />
-      </Stack.Navigator>
+      {/* Rota para o cadastro de resultados */}
+      <Stack.Screen
+        name="Register"
+        component={TotalizersScreen}
+        options={{ title: 'Cadastro de Resultado', presentation: 'modal' }}
+      />
+    </Stack.Navigator>
   );
 };
 
